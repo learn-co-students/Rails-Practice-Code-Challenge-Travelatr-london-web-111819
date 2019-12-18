2b4af51b7085d26dc5c8d3bda2bd116ce96fccc4
github username <github email address>
2019-12-18 21:58:39 +0000

    Done.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,21 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
 
+   def featured_post
+    self.posts.max_by(&:likes) #Destination.first.posts.max_by(&:likes) in console returns
+   end #andswer for a single instance
+
+  def five_recent_posts
+    self.posts.sort_by(&:created_at).reverse.take(5)
+  end 
+
+    
+    def average_age 
+      if self.bloggers.length > 0
+      self.bloggers.sum("age").to_f / self.bloggers.length
+      else
+        0
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :bloggers, only: [:index, :new, :create, :show]
+resources :posts, only: [:index, :new, :create, :show, :edit, :update]
+resources :destinations, only: [:index, :show]
+get "posts/:id/like", to: "posts#like", as: "like_post"
+
 end

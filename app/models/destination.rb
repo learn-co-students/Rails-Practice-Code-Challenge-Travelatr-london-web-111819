a31bd5280004b716d52e38130ff86f95b0cfb4cf
Matthew <matthew@matthews-mbp.home>
2019-12-18 20:03:29 +0000

    Done.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,16 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
 
+    def most_recent_posts
+        self.posts.order('created_at DESC').limit(5)
+    end
+
+    def average_blogger_age
+        if self.bloggers
+            self.bloggers.map(&:age).sum.to_f / self.bloggers.length
+        else
+           "This post has no bloggers"
+        end
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
+    belongs_to :blogger
+    belongs_to :destination
+
+    validates :title, {
+        presence: true
+    }
+
+    validates :content, {
+        length: { minimum: 100 }
+    }
 
 end
